52d90684569700784fd0edc6807baf647843dc2d
morris taylor <morristaylor451@gmail.com>
2025-09-28 00:19:45 -0700

    feat(keymaps): add jk mapping for faster escape to normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Escape alternatives - use jk to return to normal mode
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode", noremap = true, silent = true })
+vim.keymap.set("v", "jk", "<ESC>", { desc = "Exit visual mode", noremap = true, silent = true })
+vim.keymap.set("c", "jk", "<C-c>", { desc = "Exit command mode", noremap = true, silent = true })
+
 -- Code Runner keybindings (similar to VS Code)
 vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { desc = "Run Code", noremap = true, silent = false })
 vim.keymap.set("n", "<F5>", ":RunCode<CR>", { desc = "Run Code (F5)", noremap = true, silent = false })
